49fe3b35f9bee1059f5be1e59309114b05d45678
Rahul <kumarrahul8416@gmail.com>
2021-10-23 15:21:29 +0530

    Initial commit

---
+++ Mywebform.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Mywebform.aspx.cs" Inherits="WebApplication1_CURD3.Mywebform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Info</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 37%;
+        }
+        .auto-style2 {
+            width: 187px;
+        }
+        .auto-style3 {
+            width: 285px;
+        }
+    </style>
+</head>
+<body style="height: 506px">
+    <form id="form1" runat="server">
+    <div>
+    <div>
+        <div style="font-size:x-large" align="center">Student Information</div>
+    </div>
+    </div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">Employee&nbsp; ID</td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Employee Name</td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Employee Age</td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Employee Address</td>
+                <td class="auto-style3">
+                    <asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem>US</asp:ListItem>
+                        <asp:ListItem>INDIA</asp:ListItem>
+                        <asp:ListItem>UK</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+        </table>
+        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+        &nbsp;&nbsp;&nbsp;<asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" Text="Read" OnClick="Button4_Click" />
+        <br />
+        &nbsp;&nbsp;<asp:Label ID="Label1" runat="server"></asp:Label>
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+        <asp:Label ID="Label3" runat="server"></asp:Label>
+&nbsp;<asp:Label ID="Label4" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:GridView ID="GridView1" runat="server" Width="342px">
+        </asp:GridView>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;
+        <br />
+        <asp:GridView ID="GridView2" runat="server" Width="343px">
+        </asp:GridView>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+&nbsp;&nbsp;
+    </form>
+</body>
+</html>
---
+++ Mywebform.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1_CURD3
+{
+    public partial class Mywebform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-2RLTC1M;Initial Catalog=EmployeeDB;Integrated Security=True");
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd=new SqlCommand("Insert into Employee_table values('"+int.Parse(TextBox1.Text)+"','"+TextBox2.Text+"','"+int.Parse(TextBox3.Text)+"','"+DropDownList1.SelectedValue+"')",con);
+            //Response.Write("<script>alert('Insteted Successfully');</script>");
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label1.ForeColor = System.Drawing.Color.Green;
+            Label1.Text = "Inserted Successfully";
+            //System.Threading.Thread.Sleep(5000);
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            LoadData();
+            //this.Label3.Visible = true;
+            
+        }
+        void LoadData()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Employee_table", con);
+            SqlDataAdapter d = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            //GridView1.Enabled=false;
+
+        }
+        void LoadData1()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Employee_table where EmpId='" + int.Parse(TextBox1.Text) + "'", con);
+            SqlDataAdapter d1 = new SqlDataAdapter(cmd);
+            DataTable dt1 = new DataTable();
+            d1.Fill(dt1);
+            GridView2.DataSource = dt1;
+            GridView2.DataBind();
+            //GridView1.Enabled = false;
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update Employee_table set Empname='"+TextBox2.Text+"',Empage='"+int.Parse(TextBox3.Text)+"',EmpAddress='"+DropDownList1.SelectedValue+"' where EmpId='"+int.Parse(TextBox1.Text)+"'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label2.ForeColor = System.Drawing.Color.Green;
+            //Response.Write("<script>alert('Data inserted successfully')</script>");
+            Label2.Text = "Updated Successfully";
+            //TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            LoadData();
+            //GridView1.Enabled = true;
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete Employee_table where EmpId='"+int.Parse(TextBox1.Text)+ "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label3.ForeColor = System.Drawing.Color.Red;
+            //Response.Write("<script>alert('Data inserted successfully')</script>");
+            Label3.Text = "Deleted Successfully";
+            //TextBox1.Text = "";
+            //TextBox2.Text = "";
+            //TextBox3.Text = "";
+            LoadData();
+            GridView1.Enabled = true;
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from  Employee_table where EmpId='" + int.Parse(TextBox1.Text) + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label4.ForeColor = System.Drawing.Color.Green;
+            //Response.Write("<script>alert('Data inserted successfully')</script>");
+            Label3.Text = "Fetched Successfully";
+            
+            //TextBox1.Text = "";
+            //TextBox2.Text = "";
+            //TextBox3.Text = "";
+            LoadData1();
+            //GridView1.Enabled = true;
+
+        }
+    }
+}
